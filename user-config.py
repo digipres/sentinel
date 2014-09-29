d9a58b9b30639b56abeb24b2e546f8e2ccdf4993
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-09-29 10:47:58 +0100

    Added basic Travis-CI harness.

---
+++ user-config.py
@@ -0,0 +1,8 @@
+mylang='en'
+family='coptr'
+usernames['coptr']['coptr'] = u"COPTR Bot"
+usernames['coptr']['en'] = u"COPTR Bot"
+console_encoding = 'utf-8'
+transliteration_target = None
+
+family_files['coptr'] = 'http://coptr.digipres.org/api.php'
