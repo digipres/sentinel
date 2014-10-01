00223923022254f5e15fcd6e38614c9d2d7b9ee3
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-01 17:48:34 +0100

    Try global ID.

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,10 @@ cd digipres.github.io
 VERSION=$(git describe --always --tag)
 
 echo -e "\nDeploying into gh-pages branch:"
+git pull origin master
 git add --all .
 git commit -am "New site version ${VERSION} deployed." --allow-empty 
 
-git config user.name "$GIT_NAME"
-git config user.email $GIT_EMAIL
 git config credential.helper "store --file=../git-credentials"
 echo "https://$GH_TOKEN:@github.com" > ../git-credentials
 
