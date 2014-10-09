66ce163c59c3db0753dc78e1e67241b5c1b5bf5e
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-09 15:33:54 +0100

    Updated TRiD.

---
+++ registries/trid/download-trid-data.py
@@ -0,0 +1,12 @@
+from __future__ import with_statement
+from __future__ import print_function
+import urllib
+import os
+
+# Download TRiD XML sigs:
+url = "http://mark0.net/download/triddefs_xml.rar"
+urllib.urlretrieve(url,"triddefs_xml.rar")
+
+# Unpack them:
+command = "unrar x -o+ triddefs_xml.rar triddefs_xml"
+os.system(command)
