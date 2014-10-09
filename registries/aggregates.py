e148bd7041eb656fb5eba3c749e23e7676313a73
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-09 23:03:39 +0100

    Added initial aggregated data and 'linting' results.

--- registries/aggregates.py
+++ registries/aggregates.py
@@ -3,17 +3,65 @@ from __future__ import print_function
 from BeautifulSoup import BeautifulSoup
 import yaml
 import os
+import re
+
+
+def parse_mime_type(mime_type):
+    """Carves up a mime_type and returns a tuple of the
+       (type, subtype, params) where 'params' is a dictionary
+       of all the parameters for the media range.
+       For example, the media range 'application/xhtml;q=0.5' would
+       get parsed into:
+
+       ('application', 'xhtml', {'q', '0.5'})
+       """
+    parts = mime_type.split(";")
+    params = dict([tuple([s.strip() for s in param.split("=")]) for param in parts[1:] ])
+    (type, subtype) = parts[0].split("/")
+    return (type.strip(), subtype.strip(), params)
 
 def addEntry(results,key,rid,fid,name):
     if not key in results:
-        results[key] = {}
-        results[key][rid] = {}
-    if not fid in results[key][rid]:
-        results[key][rid] = {}
-        results[key][rid]['name'] = name
+        results[key] = []
+    results[key].append("%s:%s" % ( rid, fid ) )
+
+def addFormat(rid,fid,name,hasMagic,extensions,mimetypes):
+    # Add to the formats list:
+    if not rid in fmts:
+        fmts[rid] = {}
+        fmts[rid]['formats'] = {}
+        fmts[rid]['errors'] = []
+        fmts[rid]['warnings'] = []
+    if not fid in fmts[rid]['formats']:
+        fmts[rid]['formats'][fid] = {}
+    # Populate:
+    fmts[rid]['formats'][fid]['name'] = name
+    if name != name.strip():
+        fmts[rid]['warnings'].append("Format name '%s' for entry %s contains leading and/or trailing whitespace." % (name,fid))
+    fmts[rid]['formats'][fid]['hasMagic'] = hasMagic
+    fmts[rid]['formats'][fid]['extensions'] = extensions
+    fmts[rid]['formats'][fid]['mimetypes'] = mimetypes
+
+    # Assemble cross-references:
+    ext_pattern = re.compile("^[A-Za-z0-9_][A-Za-z0-9_\.]*$")
+    for extension in extensions:
+        addEntry(exts,extension,rid,fid,name)
+        # Also attempt to validate:
+        if extension.startswith("."):
+            fmts[rid]['errors'].append("File extension '%s' for entry %s should not start with a '.' (by convention)." % (extension,fid))
+        elif not ext_pattern.match(extension):
+            fmts[rid]['warnings'].append("File extension '%s' for entry %s does not appear to be a valid file extension." % (extension,fid))
+    for mimetype in mimetypes:
+        addEntry(mimes,mimetype,rid,fid,name)
+        # Also attempt to parse:
+        try:
+            (type, subtype, params) = parse_mime_type(mimetype)
+        except:
+            fmts[rid]['errors'].append("Could not parse MIME type '%s' for entry %s" % (mimetype,fid))
 
 
-def aggregateFDD(exts,mimes):
+
+def aggregateFDD():
     rid = "fdd"
     for filename in os.listdir('fdd/fddXML'):
         if filename.endswith(".xml"):
@@ -21,14 +69,25 @@ def aggregateFDD(exts,mimes):
             with open('fdd/fddXML/'+filename, "r") as f:
                 soup = BeautifulSoup(f.read())
                 ffd_id = soup.find('fdd:fdd').get('id')
+                fname = soup.find('fdd:fdd').get('titlename')
+                if soup.find('fdd:magicnumbers'):
+                    hasMagic = True
+                else:
+                    hasMagic = False
+                # Get extensions:
+                extensions = list()
                 for fe in soup.findAll('fdd:filenameextension'):
                     for fev in fe.findAll('fdd:sigvalue'):
-                        print(ffd_id,fev.text)
+                        extensions.append(fev.text)
+                # Get MIME types:
+                mimetypes = list()
+                for imts in soup.findAll('fdd:internetmediatype'):
+                    for mt in imts.findAll('fdd:sigvalue'):
+                        mimetypes.append(mt.text)
+                addFormat(rid,ffd_id,fname,hasMagic,extensions,mimetypes)
 
 
-
-
-def aggregatePronom(exts,mimes):
+def aggregatePronom():
     rid = "pronom"
 
     # Get identifiers from DROID BinSigs:
@@ -37,27 +96,40 @@ def aggregatePronom(exts,mimes):
         for ff in soup.findAll('fileformat'):
             puid = ff.get('puid')
             # Build the name:
-            pname = ff.get('name')
+            fname = ff.get('name')
             if ff.get('version') != None:
-                pname += " " + ff.get('version')
+                fname += " " + ff.get('version')
+            # Has Magic?
+            if ff.find('internalsignatureid'):
+                hasMagic = True
+            else:
+                hasMagic = False
             # Look for extensions:
+            extensions = list()
             for ext in ff.findAll('extension'):
-                extension = ext.text
-                addEntry(exts,extension,rid,puid,pname)
+                extensions.append(ext.text)
             # Look for MIME Types:
+            mimetypes = list()
             if ff.get('mimetype') != None:
                 for mime in ff.get('mimetype').split(", "):
-                    addEntry(mimes,mime,rid,puid,pname)
+                    mimetypes.append(mime)
+            addFormat(rid,puid,fname,hasMagic,extensions,mimetypes)
 
 
+# Set up hashtables to fill:
 exts = {}
 mimes = {}
+fmts = {}
 
-#aggregatePronom(exts,mimes)
-aggregateFDD(exts,mimes)
+# Grab the data:
+aggregatePronom()
+aggregateFDD()
 
 # Write out as a data file to feed into other systems:
-with open("extensions.yml", 'w') as outfile:
+for fmt in fmts:
+    with open("aggregated/formats/%s.yml" % fmt, 'w') as outfile:
+        outfile.write( yaml.safe_dump(fmts[fmt], default_flow_style=False) ) 
+with open("aggregated/extensions.yml", 'w') as outfile:
     outfile.write( yaml.safe_dump(exts, default_flow_style=False) ) 
-with open("mimetypes.yml", 'w') as outfile:
+with open("aggregated/mimetypes.yml", 'w') as outfile:
     outfile.write( yaml.safe_dump(mimes, default_flow_style=False) ) 
