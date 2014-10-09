ed23fcc5a39367ef6d5dc0b80292b296554acf73
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-09 14:59:27 +0100

    Updated sig files.

--- src/main/java/uk/bl/dpt/foreg/ForegCmd.java
+++ src/main/java/uk/bl/dpt/foreg/ForegCmd.java
@@ -11,12 +11,7 @@ import java.net.PasswordAuthentication;
 
 import javax.xml.bind.JAXBException;
 
-import org.python.core.PyInteger;
-import org.python.core.PyObject;
-import org.python.util.PythonInterpreter;
-
 import uk.gov.nationalarchives.pronom.SigFile;
-import eu.planets_project.ifr.core.techreg.formats.SigFileUtils;
 
 /**
  * @author Andrew.Jackson@bl.uk
@@ -34,41 +29,13 @@ public class ForegCmd {
 	 * @throws FileNotFoundException 
 	 */
 	public static void main(String[] args) throws Exception  {
-		//downloadSigFile();
+		downloadSigFile("registries/pronom/droid-signature-file.xml");
 		SigFileUtils.downloadAllPronomFormatRecords(new File(
 				"registries/pronom"));
-		//pythonInvoker();
+		// pythonInvoker();
 	}
 	
-	/**
-	 * See Jython Integration
-	 * http://jythonpodcast.hostjava.net/jythonbook/en/1.0/JythonAndJavaIntegration.html
-	 */
-	static void pythonInvoker() {
-		PythonInterpreter interp = new PythonInterpreter();
-        //interp.exec("import sys");
-        //interp.exec("print sys");
-        interp.set("a", new PyInteger(42));
-        interp.exec("print a");
-        interp.exec("x = 2+2");
-        PyObject x = interp.get("x");
-        System.out.println("x: " + x);
-        // Attempt to load the formats script:"
-        interp.execfile( ForegCmd.class.getResourceAsStream("formats.py"));
-        System.out.println("Hi");
-        interp.exec("item = all_formats.pop().Identifier");
-        PyObject po = interp.get("item");
-        System.out.println("Ho");
-        System.out.println("FF:"+po);
-        /* PROBLEM: Can't integrate it as it is.
-         * The barrier you are probably hitting is the method length limit in JVM
-			bytecode. It is 65535 bytes max. Long methods, really long python
-			lists or dictionaries defined in the source code, or anything like
-			that will run up against this limit.
-         */
-	}
-	
-	static void downloadSigFile() {
+	static void downloadSigFile(String outputfile) {
 		// To make java.net.URL cope with an authenticating proxy.
 		// Apache HTTPClient does this automatically, but we're not using that here at the moment.
 		String proxyUser = System.getProperty("http.proxyUser");
@@ -79,7 +46,8 @@ public class ForegCmd {
 		
 		SigFile sigFile = SigFileUtils.getLatestSigFile();
 		try {
-			SigFileUtils.writeSigFileToOutputStream(sigFile, new FileOutputStream("signaturefile.xml"));
+			SigFileUtils.writeSigFileToOutputStream(sigFile,
+					new FileOutputStream(outputfile));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
