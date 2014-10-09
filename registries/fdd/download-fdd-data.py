299d66c656927be8ffa4808252e4113fc770ef1a
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-09 15:37:19 +0100

    Added downloader script.

---
+++ registries/fdd/download-fdd-data.py
@@ -0,0 +1,12 @@
+from __future__ import with_statement
+from __future__ import print_function
+import urllib
+import os
+
+# Download TRiD XML sigs:
+url = "http://www.digitalpreservation.gov/formats/fddXML.zip"
+urllib.urlretrieve(url,"fddXML.zip")
+
+# Unpack them:
+command = "unzip -o fddXML.zip"
+os.system(command)
