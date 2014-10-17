623aa626e5edb4b8c45755e303150a57c52c88d7
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-17 10:08:25 +0100

    Added latest data.

---
+++ registries/refresh-sources.sh
@@ -0,0 +1,18 @@
+cd fdd
+python download-fdd-data.py
+cd ..
+
+cd pronom
+python download-pronom-data.py
+cd ..
+
+cd tika
+python download-tika-data.py
+cd ..
+
+cd trid
+python download-trid-data.py
+cd ..
+
+git status
+
