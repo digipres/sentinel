7f495a829519cdfafa320cc9043b8f334a56b81a
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-11-14 17:16:38 +0000

    Ensure data in submodule is up to date before overwriting.

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
+# Make sure the branch is up to date
+cd digipres.github.io
+git checkout master
+git pull origin master
+cd ..
 # Update tool info etc.
 python projections/download-coptr-tools.py
 python projections/tools-to-grid.py
