a2e2bb1938e702bf13a041618961b1bbf3ef08be
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-11-14 13:26:08 +0000

    Attempting to avoid detached head.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cd digipres.github.io
 VERSION=$(git describe --always --tag)
 
 echo -e "\nDeploying into master branch:"
+git checkout master
 git pull origin master
 git add --all .
 git commit -am "New site version ${VERSION} deployed." --allow-empty 
@@ -15,3 +16,4 @@ echo "https://$GH_TOKEN:@github.com" > ../git-credentials
 
 echo -e "\nPushing to master..."
 git push origin master
+echo -e "\n DONE."
