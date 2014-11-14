acdc5e071e403e44c5ed634ff38d649a23a40141
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-11-14 22:11:32 +0000

    Modified to generate overall tool stats.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 set -e
 
+# This all happens in here:
 cd digipres.github.io
 
+# Grab a version ID:
 VERSION=$(git describe --always --tag)
 
 echo -e "\nDeploying into master branch:"
+# Just in case something changed while we generated the data:
 git checkout master
 git pull origin master
+# Add the new stuff:
 git add --all .
 git commit -am "New site version ${VERSION} deployed." --allow-empty 
 
+# Set up the credentials for digipres.github.io
 git config credential.helper "store --file=../git-credentials"
 echo "https://$GH_TOKEN:@github.com" > ../git-credentials
 
+# And PUSH IT
 echo -e "\nPushing to master..."
 git push origin master
 echo -e "\n DONE."
--- projections/tools-to-grid.py
+++ projections/tools-to-grid.py
@@ -14,10 +14,19 @@ import string
 import sys
 import collections
 import yaml
+import datetime
 from yaml.representer import Representer
+from sets import Set
 
 with open('digipres.github.io/_data/tools/tools.yml', 'r') as infile:
     toolgrid = yaml.load( infile ) 
+
+# Count the number of tools:
+stats = {}
+stats['num_tools'] = len(toolgrid)
+subcats = Set()
+topcats = Set()
+ctypes = Set()
 
 # Loop through the tool lists and count the matrix:
 tools_matrix = {}
@@ -29,10 +38,18 @@ for tool in toolgrid:
             tools_matrix[ct]['topcats'] = collections.defaultdict(int)
             tools_matrix[ct]['subcats'] = collections.defaultdict(int)
         tools_matrix[ct]['count'] += 1
+        ctypes.add(ct)
         for cat in tool['topcats']:
             tools_matrix[ct]['topcats'][cat] += 1
+            topcats.add(cat)
         for cat in tool['subcats']:
             tools_matrix[ct]['subcats'][cat] += 1
+            subcats.add(cat)
+
+# Count the types etc:
+stats['num_topcats'] = len(topcats)
+stats['num_subcats'] = len(subcats)
+stats['num_ctypes'] = len(ctypes)
 
 def dictify(d):
     return {k:dictify(v) for k,v in d.items()} if \
@@ -44,3 +61,39 @@ for row in tools_matrix:
 
 with open('digipres.github.io/_data/tools/tool_matrix.yml', 'w') as outfile:
     outfile.write( yaml.safe_dump(tools_matrix, default_flow_style=False) ) 
+
+# Load last summary
+sf = 'digipres.github.io/_data/tools/summary.yml'
+if os.path.isfile(sf):
+    stream = open(sf, 'r')
+    summary = yaml.load(stream)
+    stream.close()
+else:
+    summary = []
+# Reset if it went wrong
+if summary == None:
+    summary = []
+
+# Check if anything has changed:
+if len(summary) == 0:
+    new_data = True
+else:
+    new_data = False
+    for key in stats:
+        if stats[key] != summary[-1]['stats'][key]:
+            new_data = True
+            break
+
+# Updated the summary stats:
+if new_data: 
+    i = datetime.datetime.now()
+    summary.append( {
+        'timestamp' : i.isoformat(),
+        'stats' : stats 
+    } )
+    # And write out:
+    with open(sf, 'w') as outfile:
+        outfile.write( yaml.safe_dump(summary, default_flow_style=False) ) 
+
+
+
