da2ff3bdd1e6710422fae5a818f30f1cd71ca6a6
Andy Jackson <anj@anjackson.net>
2022-09-01 14:11:33 +0100

    Fix secret name.

--- decrypt_secrets.sh
+++ decrypt_secrets.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Based on https://docs.github.com/en/actions/security-guides/encrypted-secrets#storing-large-secrets
+
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$FFS_PASSWORDFILE_SECRET" \
+gpg --quiet --batch --yes --decrypt --passphrase="$FFW_PASSWORDFILE_SECRET" \
 --output passwordfile passwordfile.gpg
